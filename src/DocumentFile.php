e12c7c948183decf1a3a821ce9feb91715e54ca9
EmilyS <emily.souza.rs@gmail.com>
2021-03-02 15:49:30 -0300

    Converted DocumentFile to read only

--- src/DocumentFile.php
+++ src/DocumentFile.php
@@ -7,23 +7,73 @@ namespace Lacuna\RestPki;
  * Class DocumentFile
  * @package Lacuna\RestPki
  *
- * @property string $name
- * @property string $length
- * @property string $contentType
- * @property string $location
+ * @property-read string $name
+ * @property-read string $length
+ * @property-read string $contentType
+ * @property-read string $location
  */
 class DocumentFile
 {
-    public $name;
-    public $length;
-    public $contentType;
-    public $location;
+    public $_name;
+    public $_length;
+    public $_contentType;
+    public $_location;
 
     public function __construct($model)
     {
-        $this->name = $model->name;
-        $this->length = $model->length;
-        $this->contentType = $model->contentType;
-        $this->location = $model->location;
+        $this->_name = $model->name;
+        $this->_length = $model->length;
+        $this->_contentType = $model->contentType;
+        $this->_location = $model->location;
+    }
+
+    public function getName(){
+        return $this->_name;
+    }
+
+    public function getLength(){
+        return $this->_length;
+    }
+
+    public function getContentType(){
+        return $this->_contentType;
+    }
+
+    public function getLocation(){
+        return $this->_location;
+    }
+
+    public function __get($name)
+    {
+        switch ($name) {
+            case "name":
+                return $this->getName();
+            case "length":
+                return $this->getLength();
+            case "contentType":
+                return $this->getContentType();
+            case "location":
+                return $this->getLocation();
+            default:
+                trigger_error('Undefined property: ' . __CLASS__ . '::$' . $name);
+                return null;
+        }
+    }
+
+    public function __isset($name)
+    {
+        switch ($name) {
+            case "name":
+                return isset($this->_name);
+            case "length":
+                return isset($this->_length);
+            case "contentType":
+                return isset($this->_contentType);
+            case "location":
+                return isset($this->_location);
+            default:
+                trigger_error('Undefined property: ' . __CLASS__ . '::$' . $name);
+                return null;
+        }
     }
 }
